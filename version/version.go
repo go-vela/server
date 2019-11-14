9a32ffa0bd0389a0cd224e6044474e88f8b0a246
Jordan Brockopp <Jordan.Brockopp@target.com>
2019-11-14 15:15:00 -0600

    build(version): upgrade version to v0.1.5

--- version/version.go
+++ version/version.go
@@ -12,7 +12,7 @@ var (
 	// VersionMinor is for functionality in a backwards-compatible manner
 	VersionMinor int64 = 1
 	// VersionPatch is for backwards-compatible bug fixes
-	VersionPatch int64 = 4
+	VersionPatch int64 = 5
 	// VersionDev indicates drone build number. Releases will be empty string.
 	VersionDev string
 )
