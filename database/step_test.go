593c3dce99d768021f2a836011c18cee62d96622
Emmanuel Meinen <emmanuel.meinen@target.com>
2019-11-22 14:50:57 -0600

    feat(database): add image column to database of structs (#45)
    * feat(database): add image column to database of structs
    
    * Pull in changes from master
    
    * Pull in latest types
    
    * Clean go.sum
    
    * Use varchar(500) and fix indentation
    
    * Clean up compose syntax

--- database/step_test.go
+++ database/step_test.go
@@ -38,6 +38,7 @@ func TestDatabase_Client_GetStep(t *testing.T) {
 	want.SetBuildID(1)
 	want.SetNumber(1)
 	want.SetName("foo")
+	want.SetImage("baz")
 
 	// setup database
 	db, _ := NewTest()
@@ -67,6 +68,7 @@ func TestDatabase_Client_GetStepList(t *testing.T) {
 	sOne.SetBuildID(1)
 	sOne.SetNumber(1)
 	sOne.SetName("foo")
+	sOne.SetImage("baz")
 
 	sTwo := testStep()
 	sTwo.SetID(2)
@@ -74,6 +76,7 @@ func TestDatabase_Client_GetStepList(t *testing.T) {
 	sTwo.SetBuildID(1)
 	sTwo.SetNumber(2)
 	sTwo.SetName("bar")
+	sTwo.SetImage("baz")
 
 	want := []*library.Step{sOne, sTwo}
 
@@ -111,6 +114,7 @@ func TestDatabase_Client_GetBuildStepList(t *testing.T) {
 	sOne.SetBuildID(1)
 	sOne.SetNumber(1)
 	sOne.SetName("foo")
+	sOne.SetImage("baz")
 
 	sTwo := testStep()
 	sTwo.SetID(2)
@@ -118,6 +122,7 @@ func TestDatabase_Client_GetBuildStepList(t *testing.T) {
 	sTwo.SetBuildID(1)
 	sTwo.SetNumber(2)
 	sTwo.SetName("bar")
+	sTwo.SetImage("baz")
 
 	want := []*library.Step{sTwo, sOne}
 
@@ -155,6 +160,7 @@ func TestDatabase_Client_GetBuildStepCount(t *testing.T) {
 	sOne.SetBuildID(1)
 	sOne.SetNumber(1)
 	sOne.SetName("foo")
+	sOne.SetImage("baz")
 
 	sTwo := testStep()
 	sTwo.SetID(2)
@@ -162,6 +168,7 @@ func TestDatabase_Client_GetBuildStepCount(t *testing.T) {
 	sTwo.SetBuildID(2)
 	sTwo.SetNumber(1)
 	sTwo.SetName("foo")
+	sTwo.SetImage("baz")
 
 	sThree := testStep()
 	sThree.SetID(3)
@@ -169,6 +176,7 @@ func TestDatabase_Client_GetBuildStepCount(t *testing.T) {
 	sThree.SetBuildID(2)
 	sThree.SetNumber(2)
 	sThree.SetName("bar")
+	sThree.SetImage("baz")
 
 	want := 2
 
@@ -207,6 +215,7 @@ func TestDatabase_Client_CreateStep(t *testing.T) {
 	want.SetBuildID(1)
 	want.SetNumber(1)
 	want.SetName("foo")
+	want.SetImage("baz")
 
 	// setup database
 	db, _ := NewTest()
@@ -236,6 +245,7 @@ func TestDatabase_Client_CreateStep_Invalid(t *testing.T) {
 	s.SetBuildID(1)
 	s.SetNumber(1)
 	s.SetName("foo")
+	s.SetImage("baz")
 
 	// setup database
 	db, _ := NewTest()
@@ -265,6 +275,7 @@ func TestDatabase_Client_UpdateStep(t *testing.T) {
 	want.SetBuildID(1)
 	want.SetNumber(1)
 	want.SetName("foo")
+	want.SetImage("baz")
 
 	// setup database
 	db, _ := NewTest()
@@ -295,6 +306,7 @@ func TestDatabase_Client_UpdateStep_Invalid(t *testing.T) {
 	s.SetBuildID(1)
 	s.SetNumber(1)
 	s.SetName("foo")
+	s.SetImage("baz")
 
 	// setup database
 	db, _ := NewTest()
@@ -320,6 +332,7 @@ func TestDatabase_Client_DeleteStep(t *testing.T) {
 	want.SetBuildID(1)
 	want.SetNumber(1)
 	want.SetName("foo")
+	want.SetImage("baz")
 
 	// setup database
 	db, _ := NewTest()
@@ -350,6 +363,7 @@ func testStep() *library.Step {
 		RepoID:       &i64,
 		Number:       &i,
 		Name:         &str,
+		Image:        &str,
 		Stage:        &str,
 		Status:       &str,
 		Error:        &str,
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/go-vela/compiler v0.1.7-0.20191119211930-181ca867dc69
-	github.com/go-vela/types v0.1.5-0.20191120155809-3124b5fd0366
+	github.com/go-vela/types v0.1.5-0.20191122193811-670c833293bf
 	github.com/google/go-github/v26 v26.1.3
 	github.com/google/uuid v1.1.1
 	github.com/hashicorp/vault/api v1.0.4
