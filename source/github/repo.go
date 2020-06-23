9f9cfcab56b58ad48d9aa71df8b362db23ab1f4c
jenmart <jen.mart@target.com>
2020-06-23 10:43:51 -0500

    fixing time

--- source/github/repo.go
+++ source/github/repo.go
@@ -25,8 +25,7 @@ func (c *client) ConfigBackoff(u *library.User, org, name, ref string) (data []b
 		if err != nil {
 			return
 		}
-
-		time.Sleep((i + 1) * time.Second)
+		time.Sleep(time.Duration(i+1) * time.Second)
 	}
 
 	return
