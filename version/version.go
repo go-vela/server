188c93b1c7a5cb456181fba345a3163c27af974e
Jordan Brockopp <jordan.brockopp@target.com>
2020-09-22 14:33:08 -0500

    release: updates for v0.6.0-rc1 (#184)

--- version/version.go
+++ version/version.go
@@ -10,9 +10,9 @@ var (
 	// VersionMajor is for an API incompatible changes.
 	VersionMajor int64
 	// VersionMinor is for functionality in a backwards-compatible manner.
-	VersionMinor int64 = 5
+	VersionMinor int64 = 6
 	// VersionPatch is for backwards-compatible bug fixes.
-	VersionPatch int64 = 1
+	VersionPatch int64
 	// VersionDev indicates build metadata. Releases will be empty string.
 	VersionDev string
 )
