6c1731a9e6b7ef420d69f422b8d14791a6afcbf0
Emmanuel Meinen <emmanuel.meinen@target.com>
2020-10-20 13:15:08 -0500

    feat: add api endpoints for managing the worker table (#193)
    * feat: add worker table
    
    * go mod tidy
    
    * import compiler latest
    
    * import latest from types
    
    * fix: worker table index targets
    
    * fix: add CreateWorker and UpdateWorker funcs to interface
    
    * change tabs to spaces
    
    * fix ddl/sqlite/worker.go to have lowercase workers table name
    
    Co-authored-by: Jordan Brockopp <jordan.brockopp@target.com>
    
    * add error check on GetWorkerCount
    
    * fix: use correct name for worker table items
    
    * fix: require unique hostnames in the worker table
    
    * fix: added comma missed in worker table (sqlite)
    
    * feat: add api endpoints for managing the worker table
    
    * fix(vault): skip auth method if token is provided
    
    * feat: allow user to set the lastCheckedIn for a worker
    
    * fix: ensure modifying worker resources requires admin
    
    * fix: typo in update worker
    
    * fix: typos in worker api comments
    
    * fix: list routes in router/worker.go
    
    * feat: add api spec for worker endpoints
    
    * fix: add updated api-spec
    
    * fix: add error check to binding input in create worker
    
    Co-authored-by: Jordan Brockopp <jordan.brockopp@target.com>
    Co-authored-by: Neal <neal.coleman@target.com>

---
+++ router/middleware/worker/worker_test.go
@@ -0,0 +1,113 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package worker
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/go-vela/server/database"
+
+	"github.com/go-vela/types/library"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestWorker_Retrieve(t *testing.T) {
+	// setup types
+	want := new(library.Worker)
+	want.SetID(1)
+
+	// setup context
+	gin.SetMode(gin.TestMode)
+	context, _ := gin.CreateTestContext(nil)
+	ToContext(context, want)
+
+	// run test
+	got := Retrieve(context)
+
+	if got != want {
+		t.Errorf("Retrieve is %v, want %v", got, want)
+	}
+}
+
+func TestWorker_Establish(t *testing.T) {
+	// setup types
+	want := new(library.Worker)
+	want.SetID(1)
+	want.SetHostname("worker_0")
+	want.SetAddress("localhost")
+	want.SetRoutes([]string{"foo", "bar", "baz"})
+	want.SetActive(true)
+	want.SetLastCheckedIn(12345)
+
+	got := new(library.Worker)
+
+	// setup database
+	db, _ := database.NewTest()
+
+	defer func() {
+		db.Database.Exec("delete from workers;")
+		db.Database.Close()
+	}()
+
+	_ = db.CreateWorker(want)
+
+	// setup context
+	gin.SetMode(gin.TestMode)
+
+	resp := httptest.NewRecorder()
+	context, engine := gin.CreateTestContext(resp)
+	context.Request, _ = http.NewRequest(http.MethodGet, "/workers/worker_0", nil)
+
+	// setup mock server
+	engine.Use(func(c *gin.Context) { database.ToContext(c, db) })
+	engine.Use(Establish())
+	engine.GET("/workers/:worker", func(c *gin.Context) {
+		got = Retrieve(c)
+
+		c.Status(http.StatusOK)
+	})
+
+	// run test
+	engine.ServeHTTP(resp, context.Request)
+
+	if resp.Code != http.StatusOK {
+		t.Errorf("Establish returned %v, want %v", resp.Code, http.StatusOK)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Establish is %v, want %v", got, want)
+	}
+}
+
+func TestWorker_Establish_NoWorkerParameter(t *testing.T) {
+	// setup database
+	db, _ := database.NewTest()
+	defer db.Database.Close()
+
+	// setup context
+	gin.SetMode(gin.TestMode)
+
+	resp := httptest.NewRecorder()
+	context, engine := gin.CreateTestContext(resp)
+	context.Request, _ = http.NewRequest(http.MethodGet, "/workers/", nil)
+
+	// setup mock server
+	engine.Use(func(c *gin.Context) { database.ToContext(c, db) })
+	engine.Use(Establish())
+	engine.GET("/workers/:worker", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
+
+	// run test
+	engine.ServeHTTP(context.Writer, context.Request)
+
+	if resp.Code != http.StatusBadRequest {
+		t.Errorf("Establish returned %v, want %v", resp.Code, http.StatusBadRequest)
+	}
+}
