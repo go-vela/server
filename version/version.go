7b97229c6f0d1593a6fdab239dc6ea8a1376fa63
David May <david.may@target.com>
2020-12-28 14:59:08 -0600

    bump patch version

--- version/version.go
+++ version/version.go
@@ -12,7 +12,7 @@ var (
 	// VersionMinor is for functionality in a backwards-compatible manner.
 	VersionMinor int64 = 6
 	// VersionPatch is for backwards-compatible bug fixes.
-	VersionPatch int64
+	VersionPatch int64 = 2
 	// VersionDev indicates build metadata. Releases will be empty string.
 	VersionDev string
 )
