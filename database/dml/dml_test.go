db01ce5562a4f631e10a8e08b824dd138c7a9c0f
Jordan Brockopp <jordan.brockopp@target.com>
2021-01-20 11:01:25 -0600

    chore: update copyright year to 2021 (#253)

--- database/dml/dml_test.go
+++ database/dml/dml_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
 //
 // Use of this source code is governed by the LICENSE file in this repository.
 
