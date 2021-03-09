13f6cafac298eeab0be3ad030cf0cee6074239e2
Jordan <Jordan.Brockopp@target.com>
2021-03-08 18:50:50 -0600

    feat(source): add Setup{} for source configuration

---
+++ source/setup.go
@@ -0,0 +1,96 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package source
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-vela/types/constants"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Setup represents the configuration necessary for
+// creating a Vela service capable of integrating
+// with a configured source system.
+type Setup struct {
+	// Source Configuration
+
+	// specifies the source driver to use
+	Driver string
+	// specifies the source address to use
+	Address string
+	// specifies the OAuth client ID to use from the source system
+	ClientID string
+	// specifies the OAuth client secret to use from the source system
+	ClientSecret string
+	// specifies the Vela server address to use
+	ServerAddress string
+	// specifies the context for the commit status for the source system
+	StatusContext string
+	// specifies the Vela web UI address to use
+	WebUIAddress string
+}
+
+// Github creates and returns a Vela service capable of
+// integrating with a Github source system.
+func (s *Setup) Github() (Service, error) {
+	logrus.Trace("creating github source client from setup")
+
+	return nil, fmt.Errorf("unsupported source driver: %s", constants.DriverGithub)
+}
+
+// Gitlab creates and returns a Vela service capable of
+// integrating with a Gitlab source system.
+func (s *Setup) Gitlab() (Service, error) {
+	logrus.Trace("creating gitlab source client from setup")
+
+	return nil, fmt.Errorf("unsupported source driver: %s", constants.DriverGitlab)
+}
+
+// Validate verifies the necessary fields for the
+// provided configuration are populated correctly.
+func (s *Setup) Validate() error {
+	logrus.Trace("validating executor setup for client")
+
+	// verify a source driver was provided
+	if len(s.Driver) == 0 {
+		return fmt.Errorf("no source driver provided")
+	}
+
+	// verify a source address was provided
+	if len(s.Address) == 0 {
+		return fmt.Errorf("no source address provided")
+	}
+
+	// check if the source address has a scheme
+	if !strings.Contains(s.Address, "://") {
+		return fmt.Errorf("source address must be fully qualified (<scheme>://<host>)")
+	}
+
+	// check if the source address has a trailing slash
+	if strings.HasSuffix(s.Address, "/") {
+		return fmt.Errorf("source address must not have trailing slash")
+	}
+
+	// verify a source OAuth client ID was provided
+	if len(s.ClientID) == 0 {
+		return fmt.Errorf("no source client id provided")
+	}
+
+	// verify a source OAuth client secret was provided
+	if len(s.ClientSecret) == 0 {
+		return fmt.Errorf("no source client secret provided")
+	}
+
+	// verify a source status context secret was provided
+	if len(s.StatusContext) == 0 {
+		return fmt.Errorf("no source status context provided")
+	}
+
+	// setup is valid
+	return nil
+}
---
+++ source/setup_test.go
@@ -0,0 +1,189 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package source
+
+import (
+	"testing"
+)
+
+func TestSource_Setup_Github(t *testing.T) {
+	// setup types
+	_setup := &Setup{
+		Driver:        "github",
+		Address:       "https://github.com",
+		ClientID:      "foo",
+		ClientSecret:  "bar",
+		ServerAddress: "https://vela-server.example.com",
+		StatusContext: "continuous-integration/vela",
+		WebUIAddress:  "https://vela.example.com",
+	}
+
+	// run test
+	got, err := _setup.Github()
+	if err == nil {
+		t.Errorf("Github should have returned err")
+	}
+
+	if got != nil {
+		t.Errorf("Github is %v, want nil", got)
+	}
+}
+
+func TestSource_Setup_Gitlab(t *testing.T) {
+	// setup types
+	_setup := &Setup{
+		Driver:        "gitlab",
+		Address:       "https://gitlab.com",
+		ClientID:      "foo",
+		ClientSecret:  "bar",
+		ServerAddress: "https://vela-server.example.com",
+		StatusContext: "continuous-integration/vela",
+		WebUIAddress:  "https://vela.example.com",
+	}
+
+	// run test
+	got, err := _setup.Gitlab()
+	if err == nil {
+		t.Errorf("Gitlab should have returned err")
+	}
+
+	if got != nil {
+		t.Errorf("Gitlab is %v, want nil", got)
+	}
+}
+
+func TestSource_Setup_Validate(t *testing.T) {
+	// setup tests
+	tests := []struct {
+		failure bool
+		setup   *Setup
+	}{
+		{
+			failure: false,
+			setup: &Setup{
+				Driver:        "github",
+				Address:       "https://github.com",
+				ClientID:      "foo",
+				ClientSecret:  "bar",
+				ServerAddress: "https://vela-server.example.com",
+				StatusContext: "continuous-integration/vela",
+				WebUIAddress:  "https://vela.example.com",
+			},
+		},
+		{
+			failure: false,
+			setup: &Setup{
+				Driver:        "gitlab",
+				Address:       "https://gitlab.com",
+				ClientID:      "foo",
+				ClientSecret:  "bar",
+				ServerAddress: "https://vela-server.example.com",
+				StatusContext: "continuous-integration/vela",
+				WebUIAddress:  "https://vela.example.com",
+			},
+		},
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:        "github",
+				Address:       "https://github.com/",
+				ClientID:      "foo",
+				ClientSecret:  "bar",
+				ServerAddress: "https://vela-server.example.com",
+				StatusContext: "continuous-integration/vela",
+				WebUIAddress:  "https://vela.example.com",
+			},
+		},
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:        "github",
+				Address:       "github.com",
+				ClientID:      "foo",
+				ClientSecret:  "bar",
+				ServerAddress: "https://vela-server.example.com",
+				StatusContext: "continuous-integration/vela",
+				WebUIAddress:  "https://vela.example.com",
+			},
+		},
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:        "",
+				Address:       "https://github.com",
+				ClientID:      "foo",
+				ClientSecret:  "bar",
+				ServerAddress: "https://vela-server.example.com",
+				StatusContext: "continuous-integration/vela",
+				WebUIAddress:  "https://vela.example.com",
+			},
+		},
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:        "github",
+				Address:       "",
+				ClientID:      "foo",
+				ClientSecret:  "bar",
+				ServerAddress: "https://vela-server.example.com",
+				StatusContext: "continuous-integration/vela",
+				WebUIAddress:  "https://vela.example.com",
+			},
+		},
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:        "github",
+				Address:       "https://github.com",
+				ClientID:      "",
+				ClientSecret:  "bar",
+				ServerAddress: "https://vela-server.example.com",
+				StatusContext: "continuous-integration/vela",
+				WebUIAddress:  "https://vela.example.com",
+			},
+		},
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:        "github",
+				Address:       "https://github.com",
+				ClientID:      "foo",
+				ClientSecret:  "",
+				ServerAddress: "https://vela-server.example.com",
+				StatusContext: "continuous-integration/vela",
+				WebUIAddress:  "https://vela.example.com",
+			},
+		},
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:        "github",
+				Address:       "https://github.com",
+				ClientID:      "foo",
+				ClientSecret:  "bar",
+				ServerAddress: "https://vela-server.example.com",
+				StatusContext: "",
+				WebUIAddress:  "https://vela.example.com",
+			},
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		err := test.setup.Validate()
+
+		if test.failure {
+			if err == nil {
+				t.Errorf("Validate should have returned err")
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("Validate returned err: %v", err)
+		}
+	}
+}
