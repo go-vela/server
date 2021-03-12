6188a5ab31d071f85f7382ffdf9e77ed30823c3e
Jordan Brockopp <jordan.brockopp@target.com>
2021-03-12 12:52:33 -0600

    feat(source): add Setup{} and New() to accept *Setup{} (#310)
    * refactor(source): source.go -> service.go
    
    * feat(source): add Setup{} for source configuration
    
    * feat(source): add New(*Setup{}) to create source client
    
    * chore: fix typos in comments
    
    * docs: update setup field comments

--- source/source.go
+++ source/source.go
@@ -5,111 +5,45 @@
 package source
 
 import (
-	"net/http"
+	"fmt"
 
-	"github.com/go-vela/types"
-	"github.com/go-vela/types/library"
+	"github.com/go-vela/types/constants"
+
+	"github.com/sirupsen/logrus"
 )
 
-// Service represents the interface for Vela integrating
-// with the different supported source providers.
-type Service interface {
-	// Authentication Source Interface Functions
-
-	// Authorize defines a function that uses the
-	// given access token to authorize the user.
-	Authorize(string) (string, error)
-	// Authenticate defines a function that completes
-	// the OAuth workflow for the session.
-	Authenticate(http.ResponseWriter, *http.Request, string) (*library.User, error)
-
-	// AuthenticateToken defines a function that completes
-	// the OAuth workflow for the session using PAT Token
-	AuthenticateToken(*http.Request) (*library.User, error)
-
-	// Login defines a function that begins
-	// the OAuth workflow for the session.
-	Login(http.ResponseWriter, *http.Request) (string, error)
-
-	// Access Source Interface Functions
-
-	// OrgAccess defines a function that captures
-	// the user's access level for an org.
-	OrgAccess(*library.User, string) (string, error)
-	// RepoAccess defines a function that captures
-	// the user's access level for a repo.
-	RepoAccess(*library.User, string, string) (string, error)
-	// TeamAccess defines a function that captures
-	// the user's access level for a team.
-	TeamAccess(*library.User, string, string) (string, error)
-
-	// Changeset Source Interface Functions
-
-	// Changeset defines a function that captures the list
-	// of files changed for a commit.
+// nolint: godot // top level comment ends in a list
+//
+// New creates and returns a Vela service capable of
+// integrating with the configured source provider.
+//
+// Currently the following source providers are supported:
+//
+// * Github
+func New(s *Setup) (Service, error) {
+	// validate the setup being provided
 	//
-	// https://en.wikipedia.org/wiki/Changeset.
-	Changeset(*library.User, *library.Repo, string) ([]string, error)
-	// ChangesetPR defines a function that captures the list
-	// of files changed for a pull request.
-	//
-	// https://en.wikipedia.org/wiki/Changeset.
-	ChangesetPR(*library.User, *library.Repo, int) ([]string, error)
+	// https://pkg.go.dev/github.com/go-vela/server/source?tab=doc#Setup.Validate
+	err := s.Validate()
+	if err != nil {
+		return nil, err
+	}
 
-	// Deployment Source Interface Functions
-
-	// GetDeployment defines a function that
-	// gets a deployment by number and repo.
-	GetDeployment(*library.User, *library.Repo, int64) (*library.Deployment, error)
-	// GetDeploymentCount defines a function that
-	// counts a list of all deployment for a repo.
-	GetDeploymentCount(*library.User, *library.Repo) (int64, error)
-	// GetDeploymentList defines a function that gets
-	// a list of all deployments for a repo.
-	GetDeploymentList(*library.User, *library.Repo, int, int) ([]*library.Deployment, error)
-	// CreateDeployment defines a function that
-	// creates a new deployment.
-	CreateDeployment(*library.User, *library.Repo, *library.Deployment) error
-
-	// Repo Source Interface Functions
-
-	// Config defines a function that captures
-	// the pipeline configuration from a repo.
-	Config(*library.User, string, string, string) ([]byte, error)
-	// ConfigBackoff is a truncated constant backoff wrapper for Config.
-	// Retry again in five seconds if Config fails to retrieve yaml/yml file.
-	// Will return an error after five failed attempts.
-	ConfigBackoff(*library.User, string, string, string) ([]byte, error)
-	// Disable defines a function that deactivates
-	// a repo by destroying the webhook.
-	Disable(*library.User, string, string) error
-	// Enable defines a function that activates
-	// a repo by creating the webhook.
-	Enable(*library.User, string, string, string) (string, error)
-	// Status defines a function that sends the
-	// commit status for the given SHA from a repo.
-	Status(*library.User, *library.Build, string, string) error
-	// ListUserRepos defines a function that retrieves
-	// all repos with admin rights for the user.
-	ListUserRepos(*library.User) ([]*library.Repo, error)
-	// GetPullRequest defines a function that retrieves
-	// a pull request for a repo.
-	GetPullRequest(*library.User, *library.Repo, int) (string, string, string, string, error)
-	// GetRepo defines a function that retrieves
-	// details for a repo.
-	GetRepo(*library.User, *library.Repo) (*library.Repo, error)
-	// GetHTMLURL defines a function that retrieves
-	// a repository file's html_url.
-	GetHTMLURL(*library.User, string, string, string, string) (string, error)
-
-	// Webhook Source Interface Functions
-
-	// ProcessWebhook defines a function that
-	// parses the webhook from a repo.
-	ProcessWebhook(*http.Request) (*types.Webhook, error)
-	// VerifyWebhook defines a function that
-	// verifies the webhook from a repo.
-	VerifyWebhook(*http.Request, *library.Repo) error
-
-	// TODO: Add convert functions to interface?
+	logrus.Debug("creating source service from setup")
+	// process the source driver being provided
+	switch s.Driver {
+	case constants.DriverGithub:
+		// handle the Github source driver being provided
+		//
+		// https://pkg.go.dev/github.com/go-vela/server/source?tab=doc#Setup.Github
+		return s.Github()
+	case constants.DriverGitlab:
+		// handle the Gitlab source driver being provided
+		//
+		// https://pkg.go.dev/github.com/go-vela/server/source?tab=doc#Setup.Gitlab
+		return s.Gitlab()
+	default:
+		// handle an invalid source driver being provided
+		return nil, fmt.Errorf("invalid source driver provided: %s", s.Driver)
+	}
 }
