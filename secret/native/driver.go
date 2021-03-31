bae5e65e66561cf972bdde0aab515ad7f6fd520b
Jordan <Jordan.Brockopp@target.com>
2021-03-31 08:38:04 -0500

    feat: add secret.Driver() function

---
+++ secret/native/driver.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package native
+
+import "github.com/go-vela/types/constants"
+
+// Driver outputs the configured secret driver.
+func (c *client) Driver() string {
+	return constants.DriverNative
+}
