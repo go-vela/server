f6d8431ff556e2f694f6bc96eabb15e74622bfea
Jordan <Jordan.Brockopp@target.com>
2021-03-31 08:27:22 -0500

    feat: add source.Driver() function

---
+++ source/github/driver.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package github
+
+import "github.com/go-vela/types/constants"
+
+// Driver outputs the configured source driver.
+func (c *client) Driver() string {
+	return constants.DriverGithub
+}
