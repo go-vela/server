a2821f9bd765fa3bb51f83edc25a5d098a84ee1c
Jordan <Jordan.Brockopp@target.com>
2021-05-04 08:12:01 -0500

    feat(postgres): add vela resource ddl

---
+++ database/postgres/ddl/repo.go
@@ -0,0 +1,45 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package ddl
+
+const (
+	// CreateRepoTable represents a query to
+	// create the repos table for Vela.
+	CreateRepoTable = `
+CREATE TABLE
+IF NOT EXISTS
+repos (
+	id            SERIAL PRIMARY KEY,
+	user_id       INTEGER,
+	hash          VARCHAR(500),
+	org           VARCHAR(250),
+	name          VARCHAR(250),
+	full_name     VARCHAR(500),
+	link          VARCHAR(1000),
+	clone         VARCHAR(1000),
+	branch        VARCHAR(250),
+	timeout       INTEGER,
+	visibility    TEXT,
+	private       BOOLEAN,
+	trusted       BOOLEAN,
+	active        BOOLEAN,
+	allow_pull    BOOLEAN,
+	allow_push    BOOLEAN,
+	allow_deploy  BOOLEAN,
+	allow_tag     BOOLEAN,
+	allow_comment BOOLEAN,
+	UNIQUE(full_name)
+);
+`
+
+	// CreateRepoOrgNameIndex represents a query to create an
+	// index on the repos table for the org and name columns.
+	CreateRepoOrgNameIndex = `
+CREATE INDEX
+IF NOT EXISTS
+repos_org_name
+ON repos (org, name);
+`
+)
---
+++ database/postgres/ddl/worker.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package ddl
+
+const (
+	// CreateWorkerTable represents a query to
+	// create the workers table for Vela.
+	CreateWorkerTable = `
+CREATE TABLE
+IF NOT EXISTS
+workers (
+	id               SERIAL PRIMARY KEY,
+	hostname         VARCHAR(250),
+	address          VARCHAR(250),
+	routes           VARCHAR(1000),
+	active           BOOLEAN,
+	last_checked_in  INTEGER,
+	build_limit      INTEGER,
+	UNIQUE(hostname)
+);
+`
+
+	// CreateWorkersHostnameAddressIndex represents a query to create an
+	// index on the workers table for the hostname and address columns.
+	CreateWorkersHostnameAddressIndex = `
+CREATE INDEX
+IF NOT EXISTS
+workers_hostname_address
+ON workers (hostname, address);
+`
+)
