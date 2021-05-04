98bbb9b90813d7c21893d0e09b271c880a269f2e
Jordan <Jordan.Brockopp@target.com>
2021-05-04 08:16:58 -0500

    feat(sqlite): add vela resource ddl

---
+++ database/sqlite/ddl/user.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package ddl
+
+const (
+	// CreateUserTable represents a query to
+	// create the users table for Vela.
+	CreateUserTable = `
+CREATE TABLE
+IF NOT EXISTS
+users (
+	id             INTEGER PRIMARY KEY AUTOINCREMENT,
+	name           TEXT,
+	refresh_token  TEXT,
+	token          TEXT,
+	hash           TEXT,
+	favorites      TEXT,
+	active         BOOLEAN,
+	admin          BOOLEAN,
+	UNIQUE(name)
+);
+`
+
+	// CreateRefreshIndex represents a query to create an
+	// index on the users table for the refresh_token column.
+	CreateUserRefreshIndex = `
+CREATE INDEX
+IF NOT EXISTS
+users_refresh
+ON users (refresh_token);
+`
+)
