db5db01e0784ecaaea9a8a9621d51ceb30b8775a
Jordan <Jordan.Brockopp@target.com>
2021-05-11 12:48:36 -0500

    feat(postgres): add user database functions

---
+++ database/postgres/user_count.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package postgres
+
+import (
+	"github.com/go-vela/server/database/postgres/dml"
+	"github.com/go-vela/types/constants"
+
+	"github.com/sirupsen/logrus"
+)
+
+// GetUserCount gets a list of all users from the database.
+func (c *client) GetUserCount() (int64, error) {
+	logrus.Trace("getting count of users from the database")
+
+	// variable to store query results
+	var u int64
+
+	// send query to the database and store result in variable
+	err := c.Postgres.
+		Table(constants.TableUser).
+		Raw(dml.SelectUsersCount).
+		Pluck("count", &u).Error
+
+	return u, err
+}
