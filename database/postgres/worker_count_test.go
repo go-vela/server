1c40995652b5dcb1a0d51d1f3a3b33493743bb7b
Jordan <Jordan.Brockopp@target.com>
2021-05-11 14:34:15 -0500

    feat(postgres): add worker database functions

---
+++ database/postgres/worker_count_test.go
@@ -0,0 +1,80 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package postgres
+
+import (
+	"reflect"
+	"testing"
+
+	sqlmock "github.com/DATA-DOG/go-sqlmock"
+
+	"github.com/go-vela/server/database/postgres/dml"
+)
+
+func TestPostgres_Client_GetWorkerCount(t *testing.T) {
+	// setup types
+	_workerOne := testWorker()
+	_workerOne.SetID(1)
+	_workerOne.SetHostname("worker_0")
+	_workerOne.SetAddress("localhost")
+	_workerOne.SetActive(true)
+
+	_workerTwo := testWorker()
+	_workerTwo.SetID(2)
+	_workerTwo.SetHostname("worker_1")
+	_workerTwo.SetAddress("localhost")
+	_workerTwo.SetActive(true)
+
+	// create the new fake sql database
+	_sql, _mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Errorf("unable to create new sql mock database: %v", err)
+	}
+	defer _sql.Close()
+
+	// create expected return in mock
+	_rows := sqlmock.NewRows([]string{"count"}).AddRow(2)
+
+	// ensure the mock expects the query
+	_mock.ExpectQuery(dml.SelectWorkersCount).WillReturnRows(_rows)
+
+	// setup the database client
+	_database, err := NewTest(_sql)
+	if err != nil {
+		t.Errorf("unable to create new postgres test database: %v", err)
+	}
+
+	// setup tests
+	tests := []struct {
+		failure bool
+		want    int64
+	}{
+		{
+			failure: false,
+			want:    2,
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		got, err := _database.GetWorkerCount()
+
+		if test.failure {
+			if err == nil {
+				t.Errorf("GetWorkerCount should have returned err")
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("GetWorkerCount returned err: %v", err)
+		}
+
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("GetWorkerCount is %v, want %v", got, test.want)
+		}
+	}
+}
