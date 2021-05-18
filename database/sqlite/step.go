c27f64899782059917d4cb9e170734e319a4de6a
Jordan <Jordan.Brockopp@target.com>
2021-05-18 11:47:59 -0500

    feat(sqlite): add step database functions

---
+++ database/sqlite/step.go
@@ -0,0 +1,78 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package sqlite
+
+import (
+	"github.com/go-vela/server/database/sqlite/dml"
+	"github.com/go-vela/types/constants"
+	"github.com/go-vela/types/database"
+	"github.com/go-vela/types/library"
+
+	"github.com/sirupsen/logrus"
+)
+
+// GetStep gets a step by number and build ID from the database.
+func (c *client) GetStep(number int, b *library.Build) (*library.Step, error) {
+	logrus.Tracef("getting step %d for build %d from the database", number, b.GetNumber())
+
+	// variable to store query results
+	s := new(database.Step)
+
+	// send query to the database and store result in variable
+	err := c.Sqlite.
+		Table(constants.TableStep).
+		Raw(dml.SelectBuildStep, b.GetID(), number).
+		Scan(s).Error
+
+	return s.ToLibrary(), err
+}
+
+// CreateStep creates a new step in the database.
+func (c *client) CreateStep(s *library.Step) error {
+	logrus.Tracef("creating step %s in the database", s.GetName())
+
+	// cast to database type
+	step := database.StepFromLibrary(s)
+
+	// validate the necessary fields are populated
+	err := step.Validate()
+	if err != nil {
+		return err
+	}
+
+	// send query to the database
+	return c.Sqlite.
+		Table(constants.TableStep).
+		Create(step).Error
+}
+
+// UpdateStep updates a step in the database.
+func (c *client) UpdateStep(s *library.Step) error {
+	logrus.Tracef("updating step %s in the database", s.GetName())
+
+	// cast to database type
+	step := database.StepFromLibrary(s)
+
+	// validate the necessary fields are populated
+	err := step.Validate()
+	if err != nil {
+		return err
+	}
+
+	// send query to the database
+	return c.Sqlite.
+		Table(constants.TableStep).
+		Save(step).Error
+}
+
+// DeleteStep deletes a step by unique ID from the database.
+func (c *client) DeleteStep(id int64) error {
+	logrus.Tracef("deleting step %d from the database", id)
+
+	// send query to the database
+	return c.Sqlite.
+		Table(constants.TableStep).
+		Exec(dml.DeleteStep, id).Error
+}
---
+++ database/sqlite/step_count.go
@@ -0,0 +1,89 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package sqlite
+
+import (
+	"github.com/go-vela/server/database/sqlite/dml"
+	"github.com/go-vela/types/constants"
+	"github.com/go-vela/types/library"
+
+	"github.com/sirupsen/logrus"
+)
+
+// GetBuildStepCount gets a count of all steps by build ID from the database.
+func (c *client) GetBuildStepCount(b *library.Build) (int64, error) {
+	logrus.Tracef("getting count of steps for build %d from the database", b.GetNumber())
+
+	// variable to store query results
+	var s int64
+
+	// send query to the database and store result in variable
+	err := c.Sqlite.
+		Table(constants.TableStep).
+		Raw(dml.SelectBuildStepsCount, b.GetID()).
+		Pluck("count", &s).Error
+
+	return s, err
+}
+
+// GetStepImageCount gets a count of all step images
+// and the count of their occurrence in the database.
+func (c *client) GetStepImageCount() (map[string]float64, error) {
+	logrus.Tracef("getting count of all images for steps from the database")
+
+	type imageCount struct {
+		Image string
+		Count int
+	}
+
+	// variable to store query results
+	images := new([]imageCount)
+	counts := make(map[string]float64)
+
+	// send query to the database and store result in variable
+	err := c.Sqlite.
+		Table(constants.TableStep).
+		Raw(dml.SelectStepImagesCount).
+		Scan(images).Error
+
+	for _, image := range *images {
+		counts[image.Image] = float64(image.Count)
+	}
+
+	return counts, err
+}
+
+// GetStepStatusCount gets a list of all step statuses
+// and the count of their occurrence in the database.
+func (c *client) GetStepStatusCount() (map[string]float64, error) {
+	logrus.Trace("getting count of all statuses for steps from the database")
+
+	type statusCount struct {
+		Status string
+		Count  int
+	}
+
+	// variable to store query results
+	s := new([]statusCount)
+	counts := map[string]float64{
+		"pending": 0,
+		"failure": 0,
+		"killed":  0,
+		"running": 0,
+		"success": 0,
+	}
+
+	// send query to the database and store result in variable
+	err := c.Sqlite.
+		Table(constants.TableStep).
+		Raw(dml.SelectStepStatusesCount).
+		Scan(s).Error
+
+	for _, status := range *s {
+		counts[status.Status] = float64(status.Count)
+	}
+
+	return counts, err
+}
