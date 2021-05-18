58a5706814d876ad67373cfa45e31c2dddc7eea8
Jordan <Jordan.Brockopp@target.com>
2021-05-18 11:38:15 -0500

    feat(sqlite): add secret database functions

---
+++ database/sqlite/secret.go
@@ -0,0 +1,129 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package sqlite
+
+import (
+	"fmt"
+
+	"github.com/go-vela/server/database/sqlite/dml"
+	"github.com/go-vela/types/constants"
+	"github.com/go-vela/types/database"
+	"github.com/go-vela/types/library"
+
+	"github.com/sirupsen/logrus"
+)
+
+// GetSecret gets a secret by type, org, name (repo or team) and secret name from the database.
+func (c *client) GetSecret(t, o, n, secretName string) (*library.Secret, error) {
+	logrus.Tracef("getting %s secret %s for %s/%s from the database", t, secretName, o, n)
+
+	var err error
+
+	// variable to store query results
+	s := new(database.Secret)
+
+	// send query to the database and store result in variable
+	switch t {
+	case constants.SecretOrg:
+		err = c.Sqlite.
+			Table(constants.TableSecret).
+			Raw(dml.SelectOrgSecret, o, secretName).
+			Scan(s).Error
+	case constants.SecretRepo:
+		err = c.Sqlite.
+			Table(constants.TableSecret).
+			Raw(dml.SelectRepoSecret, o, n, secretName).
+			Scan(s).Error
+	case constants.SecretShared:
+		err = c.Sqlite.
+			Table(constants.TableSecret).
+			Raw(dml.SelectSharedSecret, o, n, secretName).
+			Scan(s).Error
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	// decrypt the value for the secret
+	//
+	// https://pkg.go.dev/github.com/go-vela/types/database#Secret.Decrypt
+	err = s.Decrypt(c.config.EncryptionKey)
+	if err != nil {
+		// ensures that the change is backwards compatible
+		// by logging the error instead of returning it
+		// which allows us to fetch unencrypted secrets
+		logrus.Errorf("unable to decrypt %s secret %s for %s/%s: %v", t, secretName, o, n, err)
+
+		// return the unencrypted secret
+		return s.ToLibrary(), nil
+	}
+
+	// return the decrypted secret
+	return s.ToLibrary(), nil
+}
+
+// CreateSecret creates a new secret in the database.
+func (c *client) CreateSecret(s *library.Secret) error {
+	logrus.Tracef("creating %s secret %s in the database", s.GetType(), s.GetName())
+
+	// cast to database type
+	secret := database.SecretFromLibrary(s)
+
+	// validate the necessary fields are populated
+	err := secret.Validate()
+	if err != nil {
+		return err
+	}
+
+	// encrypt the value for the secret
+	//
+	// https://pkg.go.dev/github.com/go-vela/types/database#Secret.Encrypt
+	err = secret.Encrypt(c.config.EncryptionKey)
+	if err != nil {
+		return fmt.Errorf("unable to encrypt secret %s: %v", s.GetName(), err)
+	}
+
+	// send query to the database
+	return c.Sqlite.
+		Table(constants.TableSecret).
+		Create(secret).Error
+}
+
+// UpdateSecret updates a secret in the database.
+func (c *client) UpdateSecret(s *library.Secret) error {
+	logrus.Tracef("updating %s secret %s in the database", s.GetType(), s.GetName())
+
+	// cast to database type
+	secret := database.SecretFromLibrary(s)
+
+	// validate the necessary fields are populated
+	err := secret.Validate()
+	if err != nil {
+		return err
+	}
+
+	// encrypt the value for the secret
+	//
+	// https://pkg.go.dev/github.com/go-vela/types/database#Secret.Encrypt
+	err = secret.Encrypt(c.config.EncryptionKey)
+	if err != nil {
+		return fmt.Errorf("unable to encrypt secret %s: %v", s.GetName(), err)
+	}
+
+	// send query to the database
+	return c.Sqlite.
+		Table(constants.TableSecret).
+		Save(secret).Error
+}
+
+// DeleteSecret deletes a secret by unique ID from the database.
+func (c *client) DeleteSecret(id int64) error {
+	logrus.Tracef("Deleting secret %d from the database", id)
+
+	// send query to the database
+	return c.Sqlite.
+		Table(constants.TableSecret).
+		Exec(dml.DeleteSecret, id).Error
+}
