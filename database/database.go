11a5567bb59dba4d2f856fbb33c54194d60a6c64
Jordan <Jordan.Brockopp@target.com>
2021-06-01 08:26:02 -0500

    feat(database): add New(*Setup{}) to create database client

---
+++ database/database.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package database
+
+import (
+	"fmt"
+
+	"github.com/go-vela/types/constants"
+
+	"github.com/sirupsen/logrus"
+)
+
+// nolint: godot // top level comment ends in a list
+//
+// New creates and returns a Vela service capable of
+// integrating with the configured database provider.
+//
+// Currently the following database providers are supported:
+//
+// * Postgres
+// * Sqlite
+func New(s *Setup) (Service, error) {
+	// validate the setup being provided
+	//
+	// https://pkg.go.dev/github.com/go-vela/server/database?tab=doc#Setup.Validate
+	err := s.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	logrus.Debug("creating database service from setup")
+	// process the database driver being provided
+	switch s.Driver {
+	case constants.DriverPostgres:
+		// handle the Postgres database driver being provided
+		//
+		// https://pkg.go.dev/github.com/go-vela/server/database?tab=doc#Setup.Postgres
+		return s.Postgres()
+	case constants.DriverSqlite:
+		// handle the Sqlite database driver being provided
+		//
+		// https://pkg.go.dev/github.com/go-vela/server/database?tab=doc#Setup.Sqlite
+		return s.Sqlite()
+	default:
+		// handle an invalid database driver being provided
+		return nil, fmt.Errorf("invalid database driver provided: %s", s.Driver)
+	}
+}
---
+++ database/database_test.go
@@ -0,0 +1,84 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package database
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDatabase_New(t *testing.T) {
+	// setup tests
+	tests := []struct {
+		failure bool
+		setup   *Setup
+	}{
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:           "postgres",
+				Address:          "postgres://foo:bar@localhost:5432/vela",
+				CompressionLevel: 3,
+				ConnectionLife:   10 * time.Second,
+				ConnectionIdle:   5,
+				ConnectionOpen:   20,
+				EncryptionKey:    "A1B2C3D4E5G6H7I8J9K0LMNOPQRSTUVW",
+			},
+		},
+		{
+			failure: false,
+			setup: &Setup{
+				Driver:           "sqlite3",
+				Address:          "file::memory:?cache=shared",
+				CompressionLevel: 3,
+				ConnectionLife:   10 * time.Second,
+				ConnectionIdle:   5,
+				ConnectionOpen:   20,
+				EncryptionKey:    "A1B2C3D4E5G6H7I8J9K0LMNOPQRSTUVW",
+			},
+		},
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:           "mysql",
+				Address:          "foo:bar@tcp(localhost:3306)/vela?charset=utf8mb4&parseTime=True&loc=Local",
+				CompressionLevel: 3,
+				ConnectionLife:   10 * time.Second,
+				ConnectionIdle:   5,
+				ConnectionOpen:   20,
+				EncryptionKey:    "A1B2C3D4E5G6H7I8J9K0LMNOPQRSTUVW",
+			},
+		},
+		{
+			failure: true,
+			setup: &Setup{
+				Driver:           "postgres",
+				Address:          "",
+				CompressionLevel: 3,
+				ConnectionLife:   10 * time.Second,
+				ConnectionIdle:   5,
+				ConnectionOpen:   20,
+				EncryptionKey:    "A1B2C3D4E5G6H7I8J9K0LMNOPQRSTUVW",
+			},
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		_, err := New(test.setup)
+
+		if test.failure {
+			if err == nil {
+				t.Errorf("New should have returned err")
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("New returned err: %v", err)
+		}
+	}
+}
