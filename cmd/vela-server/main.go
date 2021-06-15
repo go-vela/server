b5d4ec06faca0586dafe89321a385ab8cc10e219
Jordan <Jordan.Brockopp@target.com>
2021-06-15 08:58:14 -0500

    feat: output version info to stdout

--- cmd/vela-server/main.go
+++ cmd/vela-server/main.go
@@ -5,6 +5,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"time"
 
@@ -19,10 +21,22 @@ import (
 
 // nolint: funlen // ignore function length due to flags
 func main() {
+	// capture application version information
+	v := version.New()
+
+	// serialize the version information as pretty JSON
+	bytes, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	// output the version information to stdout
+	fmt.Fprintf(os.Stdout, "%s\n", string(bytes))
+
 	app := cli.NewApp()
 	app.Name = "vela-server"
 	app.Action = server
-	app.Version = version.New().Semantic()
+	app.Version = v.Semantic()
 
 	app.Flags = []cli.Flag{
 		&cli.StringFlag{
