e5dd4a2c4b5e748f64df5c1f507cc6be0a975285
Jordan Brockopp <jordan.brockopp@target.com>
2021-06-24 12:54:51 -0500

    feat: add database.Driver() function (#431)
    Co-authored-by: David May <1301201+wass3r@users.noreply.github.com>

---
+++ database/sqlite/driver_test.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package sqlite
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/go-vela/types/constants"
+)
+
+func TestSqlite_Client_Driver(t *testing.T) {
+	// setup types
+	want := constants.DriverSqlite
+
+	// setup the test database client
+	_database, err := NewTest()
+	if err != nil {
+		t.Errorf("unable to create new sqlite test database: %v", err)
+	}
+	defer func() { _sql, _ := _database.Sqlite.DB(); _sql.Close() }()
+
+	// run test
+	got := _database.Driver()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Driver is %v, want %v", got, want)
+	}
+}
