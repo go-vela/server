35a73d0affec9b1a1e1fc5544a7634fb61e76849
JordanSussman <jordan_sussman@cargill.com>
2021-07-14 09:46:29 -0500

    handle stages

--- api/build.go
+++ api/build.go
@@ -263,6 +263,18 @@ func CreateBuild(c *gin.Context) {
 // skipEmptyBuild checks if the build should be skipped due to it
 // not containing any steps besides init or clone
 func skipEmptyBuild(p *pipeline.Build) string {
+	if len(p.Stages) == 1 {
+		if p.Stages[0].Name == "init" {
+			return "skipping build since only init stage found"
+		}
+	}
+
+	if len(p.Stages) == 2 {
+		if p.Stages[0].Name == "init" && p.Stages[1].Name == "clone" {
+			return "skipping build since only init and clone stages found"
+		}
+	}
+
 	if len(p.Steps) == 1 {
 		if p.Steps[0].Name == "init" {
 			return "skipping build since only init step found"
