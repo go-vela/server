6bad721de14f46740f62e8133171da26fe8535c7
David May <david.may@target.com>
2021-08-17 10:28:56 -0500

    fix(database): sort by created instead of started

--- database/sqlite/build_list.go
+++ database/sqlite/build_list.go
@@ -71,7 +71,7 @@ func (c *client) GetOrgBuildList(org string, filters map[string]string, page int
 		Select("builds.*").
 		Joins("JOIN repos ON builds.repo_id = repos.id AND repos.org = ?", org).
 		Where(filters).
-		Order("started DESC").
+		Order("created DESC").
 		Limit(perPage).
 		Offset(offset).
 		Scan(b).Error
