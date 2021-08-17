35838175b1738318f3c9520bc525f12c41880e70
David May <david.may@target.com>
2021-08-17 16:40:50 -0500

    fix: add second order to ensure order consistency

--- database/postgres/build_list.go
+++ database/postgres/build_list.go
@@ -72,6 +72,7 @@ func (c *client) GetOrgBuildList(org string, filters map[string]string, page, pe
 		Joins("JOIN repos ON builds.repo_id = repos.id and repos.org = ?", org).
 		Where(filters).
 		Order("created DESC").
+		Order("id").
 		Limit(perPage).
 		Offset(offset).
 		Scan(b).Error
