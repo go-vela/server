ac6e67ce5c45d1bd362ecf35eb3bc1bb9adb3d84
Matthew.Fevold <matthew.fevold@target.com>
2021-08-31 10:51:34 -0500

    fix: remove unused linter directives

--- api/webhook.go
+++ api/webhook.go
@@ -427,7 +427,6 @@ func PostWebhook(c *gin.Context) {
 			// send API call to set the status on the commit
 			err = source.FromContext(c).Status(u, b, r.GetOrg(), r.GetName())
 			if err != nil {
-				// nolint: lll // ignore long line length due to error message
 				logrus.Errorf("unable to set commit status for %s/%d: %v", r.GetFullName(), b.GetNumber(), err)
 			}
 
