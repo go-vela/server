7f79add4768e37c50a956ed95ff4e72982f96dc1
wass3r <wasser@gmail.com>
2021-11-26 11:39:39 -0600

    forgot to rename var

--- cmd/vela-server/queue.go
+++ cmd/vela-server/queue.go
@@ -18,12 +18,12 @@ func setupQueue(c *cli.Context) (queue.Service, error) {
 
 	// queue configuration
 	_setup := &queue.Setup{
-		Driver:       c.String("queue.driver"),
-		Address:      c.String("queue.addr"),
-		Cluster:      c.Bool("queue.cluster"),
-		Routes:       c.StringSlice("queue.routes"),
-		DefaultRoute: c.Bool("queue.routes.skip-default"),
-		Timeout:      c.Duration("queue.pop.timeout"),
+		Driver:           c.String("queue.driver"),
+		Address:          c.String("queue.addr"),
+		Cluster:          c.Bool("queue.cluster"),
+		Routes:           c.StringSlice("queue.routes"),
+		SkipDefaultRoute: c.Bool("queue.routes.skip-default"),
+		Timeout:          c.Duration("queue.pop.timeout"),
 	}
 
 	// setup the queue
