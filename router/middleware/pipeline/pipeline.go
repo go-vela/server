b666fbb0c3c6e9d63f99dbe8c6a8b8a1e54f1ab9
JordanBrockopp <Jordan_Brockopp@cargill.com>
2022-05-31 13:49:01 -0500

    fix(api/pipeline): looking up configs for older builds

--- router/middleware/pipeline/pipeline.go
+++ router/middleware/pipeline/pipeline.go
@@ -9,11 +9,14 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-vela/server/compiler"
 	"github.com/go-vela/server/database"
 	"github.com/go-vela/server/router/middleware/org"
 	"github.com/go-vela/server/router/middleware/repo"
 	"github.com/go-vela/server/router/middleware/user"
+	"github.com/go-vela/server/scm"
 	"github.com/go-vela/server/util"
+	"github.com/go-vela/types"
 	"github.com/go-vela/types/library"
 	"github.com/sirupsen/logrus"
 )
@@ -47,6 +50,8 @@ func Establish() gin.HandlerFunc {
 			return
 		}
 
+		entry := fmt.Sprintf("%s/%s", r.GetFullName(), p)
+
 		// update engine logger with API metadata
 		//
 		// https://pkg.go.dev/github.com/sirupsen/logrus?tab=doc#Entry.WithFields
@@ -55,15 +60,44 @@ func Establish() gin.HandlerFunc {
 			"pipeline": p,
 			"repo":     r.GetName(),
 			"user":     u.GetName(),
-		}).Debugf("reading pipeline %s/%s", r.GetFullName(), p)
+		}).Debugf("reading pipeline %s", entry)
 
 		pipeline, err := database.FromContext(c).GetPipelineForRepo(p, r)
-		if err != nil {
-			retErr := fmt.Errorf("unable to read pipeline %s/%s: %w", r.GetFullName(), p, err)
+		if err != nil { // assume the pipeline doesn't exist in the database yet (before pipeline support was added)
+			b, err := database.FromContext(c).GetLastCommitBuild(p, r)
+			if err != nil {
+				retErr := fmt.Errorf("unable to get build for pipeline configuration %s: %w", entry, err)
 
-			util.HandleError(c, http.StatusNotFound, retErr)
+				util.HandleError(c, http.StatusNotFound, retErr)
 
-			return
+				return
+			}
+
+			// send API call to capture the pipeline configuration file
+			config, err := scm.FromContext(c).ConfigBackoff(u, r, p)
+			if err != nil {
+				retErr := fmt.Errorf("unable to get pipeline configuration for %s: %w", entry, err)
+
+				util.HandleError(c, http.StatusNotFound, retErr)
+
+				return
+			}
+
+			// parse and compile the pipeline configuration file
+			_, pipeline, err = compiler.FromContext(c).
+				Duplicate().
+				WithBuild(b).
+				WithMetadata(c.MustGet("metadata").(*types.Metadata)).
+				WithRepo(r).
+				WithUser(u).
+				Compile(config)
+			if err != nil {
+				retErr := fmt.Errorf("unable to compile pipeline configuration for %s: %w", entry, err)
+
+				util.HandleError(c, http.StatusInternalServerError, retErr)
+
+				return
+			}
 		}
 
 		ToContext(c, pipeline)
