71240b4f1e474cbe488fd8c67afe7ffd76f466eb
Jordan Brockopp <jordan.brockopp@target.com>
2023-04-10 14:49:02 -0500

    refactor(database): move secret logic into separate package (#782)
    * feat(database): add user engine
    
    * chore(database): remove old user logic
    
    * refactor(database): restructure DDL functions
    
    * chore: updates for database user engine
    
    * feat(database): add repo engine
    
    * chore: save work
    
    * fix: Scan() -> Take()
    
    * fix: sort by for listing repos
    
    * feat(database): add worker engine
    
    * chore(database): remove old worker logic
    
    * chore: updates for database worker engine
    
    * fix: issues from merge conflict
    
    * chore: address linter feedback
    
    * chore: address linter feedback v2
    
    * feat(database): add hook engine
    
    * chore: remove Limit(1) with Take()
    
    * chore(database): remove old hook logic
    
    * chore: updates for database hook engine
    
    * refactor(database/worker): name of index
    
    * feat(database): add log engine
    
    * chore(database): remove old log logic
    
    * chore: updates for database log engine
    
    * chore: update copyright year
    
    * chore: address linter feedback
    
    * chore: address linter feedback
    
    * chore: save work
    
    * chore: save work
    
    * chore: finalize testing
    
    * chore: save work
    
    * chore: finalize changes
    
    * fix: failing tests
    
    * chore: address linter feedback
    
    * chore: add missing tests
    
    * chore: address linter feedback
    
    * chore: address review feedback
    
    * chore: address review feedback
    
    ---------
    
    Co-authored-by: Jacob Floyd <cognifloyd@gmail.com>
    Co-authored-by: Easton Crupper <65553218+ecrupper@users.noreply.github.com>

---
+++ database/secret/get_test.go
@@ -0,0 +1,91 @@
+// Copyright (c) 2023 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package secret
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-vela/types/library"
+)
+
+func TestSecret_Engine_GetSecret(t *testing.T) {
+	// setup types
+	_secret := testSecret()
+	_secret.SetID(1)
+	_secret.SetOrg("foo")
+	_secret.SetRepo("bar")
+	_secret.SetName("baz")
+	_secret.SetValue("foob")
+	_secret.SetType("repo")
+	_secret.SetCreatedAt(1)
+	_secret.SetCreatedBy("user")
+	_secret.SetUpdatedAt(1)
+	_secret.SetUpdatedBy("user2")
+
+	_postgres, _mock := testPostgres(t)
+	defer func() { _sql, _ := _postgres.client.DB(); _sql.Close() }()
+
+	// create expected result in mock
+	_rows := sqlmock.NewRows(
+		[]string{"id", "type", "org", "repo", "team", "name", "value", "images", "events", "allow_command", "created_at", "created_by", "updated_at", "updated_by"}).
+		AddRow(1, "repo", "foo", "bar", "", "baz", "foob", nil, nil, false, 1, "user", 1, "user2")
+
+	// ensure the mock expects the query
+	_mock.ExpectQuery(`SELECT * FROM "secrets" WHERE id = $1 LIMIT 1`).WithArgs(1).WillReturnRows(_rows)
+
+	_sqlite := testSqlite(t)
+	defer func() { _sql, _ := _sqlite.client.DB(); _sql.Close() }()
+
+	err := _sqlite.CreateSecret(_secret)
+	if err != nil {
+		t.Errorf("unable to create test secret for sqlite: %v", err)
+	}
+
+	// setup tests
+	tests := []struct {
+		failure  bool
+		name     string
+		database *engine
+		want     *library.Secret
+	}{
+		{
+			failure:  false,
+			name:     "postgres",
+			database: _postgres,
+			want:     _secret,
+		},
+		{
+			failure:  false,
+			name:     "sqlite3",
+			database: _sqlite,
+			want:     _secret,
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := test.database.GetSecret(1)
+
+			if test.failure {
+				if err == nil {
+					t.Errorf("GetSecret for %s should have returned err", test.name)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Errorf("GetSecret for %s returned err: %v", test.name, err)
+			}
+
+			if !reflect.DeepEqual(got, test.want) {
+				t.Errorf("GetSecret for %s is %v, want %v", test.name, got, test.want)
+			}
+		})
+	}
+}
