3aa414e46ced00ce3019f0d58120801a7c4cd233
Jordan Brockopp <jordan.brockopp@target.com>
2023-05-15 11:58:37 -0500

    refactor(api): move worker logic to separate package (#828)
    * refactor(api): move worker handlers to new package
    
    * chore: address review feedback
    
    Co-authored-by: Jacob Floyd <cognifloyd@gmail.com>
    
    ---------
    
    Co-authored-by: Jacob Floyd <cognifloyd@gmail.com>
    Co-authored-by: Kelly Merrick <kelly.merrick@target.com>
    Co-authored-by: David May <1301201+wass3r@users.noreply.github.com>
    Co-authored-by: Driscoll <43050003+chrispdriscoll@users.noreply.github.com>
    Co-authored-by: dave vader <48764154+plyr4@users.noreply.github.com>

---
+++ api/worker/delete.go
@@ -0,0 +1,70 @@
+// Copyright (c) 2023 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package worker
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-vela/server/database"
+	"github.com/go-vela/server/router/middleware/user"
+	"github.com/go-vela/server/router/middleware/worker"
+	"github.com/go-vela/server/util"
+	"github.com/sirupsen/logrus"
+)
+
+// swagger:operation DELETE /api/v1/workers/{worker} workers DeleteWorker
+//
+// Delete a worker for the configured backend
+//
+// ---
+// produces:
+// - application/json
+// parameters:
+// - in: path
+//   name: worker
+//   description: Name of the worker
+//   required: true
+//   type: string
+// security:
+//   - ApiKeyAuth: []
+// responses:
+//   '200':
+//     description: Successfully deleted of worker
+//     schema:
+//       type: string
+//   '500':
+//     description: Unable to delete worker
+//     schema:
+//       "$ref": "#/definitions/Error"
+
+// DeleteWorker represents the API handler to remove
+// a worker from the configured backend.
+func DeleteWorker(c *gin.Context) {
+	// capture middleware values
+	u := user.Retrieve(c)
+	w := worker.Retrieve(c)
+
+	// update engine logger with API metadata
+	//
+	// https://pkg.go.dev/github.com/sirupsen/logrus?tab=doc#Entry.WithFields
+	logrus.WithFields(logrus.Fields{
+		"user":   u.GetName(),
+		"worker": w.GetHostname(),
+	}).Infof("deleting worker %s", w.GetHostname())
+
+	// send API call to remove the step
+	err := database.FromContext(c).DeleteWorker(w)
+	if err != nil {
+		retErr := fmt.Errorf("unable to delete worker %s: %w", w.GetHostname(), err)
+
+		util.HandleError(c, http.StatusInternalServerError, retErr)
+
+		return
+	}
+
+	c.JSON(http.StatusOK, fmt.Sprintf("worker %s deleted", w.GetHostname()))
+}
