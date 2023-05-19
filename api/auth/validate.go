6bc30e0de4a67deac17d1f3d1fb2b2f7c5571d69
Jacob Floyd <cognifloyd@gmail.com>
2023-05-19 12:06:38 -0500

    minimize

--- api/auth/validate.go
+++ api/auth/validate.go
@@ -9,64 +9,10 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/go-vela/server/internal/token"
-	"github.com/go-vela/server/router/middleware/auth"
+	"github.com/gin-gonic/gin"
 	"github.com/go-vela/server/router/middleware/claims"
 	"github.com/go-vela/server/util"
-
-	"github.com/go-vela/types/library"
-
-	"github.com/gin-gonic/gin"
 )
-
-// swagger:operation GET /token-refresh authenticate GetRefreshAccessToken
-//
-// Refresh an access token
-//
-// ---
-// produces:
-// - application/json
-// security:
-//   - CookieAuth: []
-// responses:
-//   '200':
-//     description: Successfully refreshed a token
-//     schema:
-//       "$ref": "#/definitions/Token"
-//   '401':
-//     description: Unauthorized
-//     schema:
-//       "$ref": "#/definitions/Error"
-
-// RefreshAccessToken will return a new access token if the provided
-// refresh token via cookie is valid.
-func RefreshAccessToken(c *gin.Context) {
-	// capture the refresh token
-	// TODO: move this into token package and do it internally
-	// since we are already passsing context
-	rt, err := auth.RetrieveRefreshToken(c.Request)
-	if err != nil {
-		retErr := fmt.Errorf("refresh token error: %w", err)
-
-		util.HandleError(c, http.StatusUnauthorized, retErr)
-
-		return
-	}
-
-	tm := c.MustGet("token-manager").(*token.Manager)
-
-	// validate the refresh token and return a new access token
-	newAccessToken, err := tm.Refresh(c, rt)
-	if err != nil {
-		retErr := fmt.Errorf("unable to refresh token: %w", err)
-
-		util.HandleError(c, http.StatusUnauthorized, retErr)
-
-		return
-	}
-
-	c.JSON(http.StatusOK, library.Token{Token: &newAccessToken})
-}
 
 // swagger:operation GET /validate-token authenticate ValidateServerToken
 //
