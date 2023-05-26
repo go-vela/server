aabd8c05ae2e0cb1230d6a887f9a1d04192f9f44
ecrupper <easton.crupper12@gmail.com>
2023-05-26 14:37:52 -0500

    remove cleanBuild helper in favor of CleanBuild

--- api/build/plan.go
+++ api/build/plan.go
@@ -11,10 +11,8 @@ import (
 	"github.com/go-vela/server/api/service"
 	"github.com/go-vela/server/api/step"
 	"github.com/go-vela/server/database"
-	"github.com/go-vela/types/constants"
 	"github.com/go-vela/types/library"
 	"github.com/go-vela/types/pipeline"
-	"github.com/sirupsen/logrus"
 )
 
 // PlanBuild is a helper function to plan the build for
@@ -38,7 +36,7 @@ func PlanBuild(database database.Interface, p *pipeline.Build, b *library.Build,
 		//   of UPDATE-ing the existing build - which results in
 		//   a constraint error (repo_id, number)
 		// - do we want to update the build or just delete it?
-		cleanBuild(database, b, nil, nil, err)
+		CleanBuild(database, b, nil, nil, err)
 
 		return fmt.Errorf("unable to create new build for %s: %w", r.GetFullName(), err)
 	}
@@ -55,7 +53,7 @@ func PlanBuild(database database.Interface, p *pipeline.Build, b *library.Build,
 	services, err := service.PlanServices(database, p, b)
 	if err != nil {
 		// clean up the objects from the pipeline in the database
-		cleanBuild(database, b, services, nil, err)
+		CleanBuild(database, b, services, nil, err)
 
 		return err
 	}
@@ -64,51 +62,10 @@ func PlanBuild(database database.Interface, p *pipeline.Build, b *library.Build,
 	steps, err := step.PlanSteps(database, p, b)
 	if err != nil {
 		// clean up the objects from the pipeline in the database
-		cleanBuild(database, b, services, steps, err)
+		CleanBuild(database, b, services, steps, err)
 
 		return err
 	}
 
 	return nil
-}
-
-// cleanBuild is a helper function to kill the build
-// without execution. This will kill all resources,
-// like steps and services, for the build in the
-// configured backend.
-func cleanBuild(database database.Interface, b *library.Build, services []*library.Service, steps []*library.Step, e error) {
-	// update fields in build object
-	b.SetError(fmt.Sprintf("unable to publish to queue: %s", e.Error()))
-	b.SetStatus(constants.StatusError)
-	b.SetFinished(time.Now().UTC().Unix())
-
-	// send API call to update the build
-	err := database.UpdateBuild(b)
-	if err != nil {
-		logrus.Errorf("unable to kill build %d: %v", b.GetNumber(), err)
-	}
-
-	for _, s := range services {
-		// update fields in service object
-		s.SetStatus(constants.StatusKilled)
-		s.SetFinished(time.Now().UTC().Unix())
-
-		// send API call to update the service
-		err := database.UpdateService(s)
-		if err != nil {
-			logrus.Errorf("unable to kill service %s for build %d: %v", s.GetName(), b.GetNumber(), err)
-		}
-	}
-
-	for _, s := range steps {
-		// update fields in step object
-		s.SetStatus(constants.StatusKilled)
-		s.SetFinished(time.Now().UTC().Unix())
-
-		// send API call to update the step
-		err := database.UpdateStep(s)
-		if err != nil {
-			logrus.Errorf("unable to kill step %s for build %d: %v", s.GetName(), b.GetNumber(), err)
-		}
-	}
 }
