e7c44c2fa8302d355ef3fd0689a1fdd38935691b
JordanBrockopp <Jordan_Brockopp@cargill.com>
2023-06-19 15:32:47 -0500

    refactor(database/repo): return interface

--- database/repo/repo.go
+++ database/repo/repo.go
@@ -42,7 +42,7 @@ type (
 // New creates and returns a Vela service for integrating with repos in the database.
 //
 //nolint:revive // ignore returning unexported engine
-func New(opts ...EngineOpt) (*engine, error) {
+func New(opts ...EngineOpt) (RepoInterface, error) {
 	// create new Repo engine
 	e := new(engine)
 
