b910bfc05ab62701f98c75c055910234f21ffc16
JordanBrockopp <Jordan_Brockopp@cargill.com>
2023-06-28 10:48:12 -0500

    feat(database): add integration tests

--- database/integration_test.go
+++ database/integration_test.go
@@ -5,12 +5,65 @@
 package database
 
 import (
-	"fmt"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestDatabase_Integration(t *testing.T) {
-	fmt.Println("Hello")
+	// setup tests
+	tests := []struct {
+		failure bool
+		name    string
+		config  *config
+	}{
+		{
+			name:    "success with postgres",
+			failure: false,
+			config: &config{
+				Driver:           "postgres",
+				Address:          "postgres://vela:notARealPassword12345@localhost:5432/vela",
+				CompressionLevel: 3,
+				ConnectionLife:   10 * time.Second,
+				ConnectionIdle:   5,
+				ConnectionOpen:   20,
+				EncryptionKey:    "A1B2C3D4E5G6H7I8J9K0LMNOPQRSTUVW",
+				SkipCreation:     false,
+			},
+		},
+	}
 
-	t.Error("demonstrating failure")
+	// run tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			db, err := New(
+				WithAddress(test.config.Address),
+				WithCompressionLevel(test.config.CompressionLevel),
+				WithConnectionLife(test.config.ConnectionLife),
+				WithConnectionIdle(test.config.ConnectionIdle),
+				WithConnectionOpen(test.config.ConnectionOpen),
+				WithDriver(test.config.Driver),
+				WithEncryptionKey(test.config.EncryptionKey),
+				WithSkipCreation(test.config.SkipCreation),
+			)
+			if err != nil {
+				t.Errorf("unable to create new database engine for %s: %v", test.name, err)
+			}
+
+			driver := db.Driver()
+			if !strings.EqualFold(driver, test.config.Driver) {
+				t.Errorf("Driver() is %v, want %v", driver, test.config.Driver)
+			}
+
+			err = db.Ping()
+			if err != nil {
+				t.Errorf("unable to ping database engine for %s: %v", test.name, err)
+			}
+
+			err = db.Close()
+			if err != nil {
+				t.Errorf("unable to close database engine for %s: %v", test.name, err)
+			}
+		})
+	}
 }
