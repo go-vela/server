6874831153a63b72c3409ecfc97c29909089a9b5
Easton Crupper <65553218+ecrupper@users.noreply.github.com>
2023-08-23 11:32:01 -0400

    feat(queuing)!: build executable server-side implementation (#927)
    * init commit
    
    * adding db tests
    
    * compiled to build itinerary
    
    * add mock
    
    * itinerary -> executable
    
    * fix mock
    
    * update some extra db configs
    
    * pull in updated types
    
    * encrypt and decrypt
    
    * Update database/executable/interface.go
    
    Co-authored-by: dave vader <48764154+plyr4@users.noreply.github.com>
    
    * pesky linter
    
    * add integration test for executables
    
    * try this
    
    * with encryption resource opt
    
    * Update database/executable/opts.go
    
    Co-authored-by: dave vader <48764154+plyr4@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: dave vader <48764154+plyr4@users.noreply.github.com>
    Co-authored-by: David May <1301201+wass3r@users.noreply.github.com>

---
+++ database/executable/opts_test.go
@@ -0,0 +1,265 @@
+// Copyright (c) 2023 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package executable
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+
+	"gorm.io/gorm"
+)
+
+func TestExecutable_EngineOpt_WithClient(t *testing.T) {
+	// setup types
+	e := &engine{client: new(gorm.DB)}
+
+	// setup tests
+	tests := []struct {
+		failure bool
+		name    string
+		client  *gorm.DB
+		want    *gorm.DB
+	}{
+		{
+			failure: false,
+			name:    "client set to new database",
+			client:  new(gorm.DB),
+			want:    new(gorm.DB),
+		},
+		{
+			failure: false,
+			name:    "client set to nil",
+			client:  nil,
+			want:    nil,
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := WithClient(test.client)(e)
+
+			if test.failure {
+				if err == nil {
+					t.Errorf("WithClient for %s should have returned err", test.name)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Errorf("WithClient returned err: %v", err)
+			}
+
+			if !reflect.DeepEqual(e.client, test.want) {
+				t.Errorf("WithClient is %v, want %v", e.client, test.want)
+			}
+		})
+	}
+}
+
+func TestExecutable_EngineOpt_WithCompressionLevel(t *testing.T) {
+	// setup types
+	e := &engine{config: new(config)}
+
+	// setup tests
+	tests := []struct {
+		failure bool
+		name    string
+		level   int
+		want    int
+	}{
+		{
+			failure: false,
+			name:    "compression level set to -1",
+			level:   -1,
+			want:    -1,
+		},
+		{
+			failure: false,
+			name:    "compression level set to 0",
+			level:   0,
+			want:    0,
+		},
+		{
+			failure: false,
+			name:    "compression level set to 1",
+			level:   1,
+			want:    1,
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := WithCompressionLevel(test.level)(e)
+
+			if test.failure {
+				if err == nil {
+					t.Errorf("WithCompressionLevel for %s should have returned err", test.name)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Errorf("WithCompressionLevel returned err: %v", err)
+			}
+
+			if !reflect.DeepEqual(e.config.CompressionLevel, test.want) {
+				t.Errorf("WithCompressionLevel is %v, want %v", e.config.CompressionLevel, test.want)
+			}
+		})
+	}
+}
+
+func TestExecutable_EngineOpt_WithEncryptionKey(t *testing.T) {
+	// setup types
+	e := &engine{config: new(config)}
+
+	// setup tests
+	tests := []struct {
+		failure bool
+		name    string
+		key     string
+		want    string
+	}{
+		{
+			failure: false,
+			name:    "encryption key set",
+			key:     "A1B2C3D4E5G6H7I8J9K0LMNOPQRSTUVW",
+			want:    "A1B2C3D4E5G6H7I8J9K0LMNOPQRSTUVW",
+		},
+		{
+			failure: false,
+			name:    "encryption key not set",
+			key:     "",
+			want:    "",
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := WithEncryptionKey(test.key)(e)
+
+			if test.failure {
+				if err == nil {
+					t.Errorf("WithEncryptionKey for %s should have returned err", test.name)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Errorf("WithEncryptionKey returned err: %v", err)
+			}
+
+			if !reflect.DeepEqual(e.config.EncryptionKey, test.want) {
+				t.Errorf("WithEncryptionKey is %v, want %v", e.config.EncryptionKey, test.want)
+			}
+		})
+	}
+}
+
+func TestExecutable_EngineOpt_WithLogger(t *testing.T) {
+	// setup types
+	e := &engine{logger: new(logrus.Entry)}
+
+	// setup tests
+	tests := []struct {
+		failure bool
+		name    string
+		logger  *logrus.Entry
+		want    *logrus.Entry
+	}{
+		{
+			failure: false,
+			name:    "logger set to new entry",
+			logger:  new(logrus.Entry),
+			want:    new(logrus.Entry),
+		},
+		{
+			failure: false,
+			name:    "logger set to nil",
+			logger:  nil,
+			want:    nil,
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := WithLogger(test.logger)(e)
+
+			if test.failure {
+				if err == nil {
+					t.Errorf("WithLogger for %s should have returned err", test.name)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Errorf("WithLogger returned err: %v", err)
+			}
+
+			if !reflect.DeepEqual(e.logger, test.want) {
+				t.Errorf("WithLogger is %v, want %v", e.logger, test.want)
+			}
+		})
+	}
+}
+
+func TestExecutable_EngineOpt_WithSkipCreation(t *testing.T) {
+	// setup types
+	e := &engine{config: new(config)}
+
+	// setup tests
+	tests := []struct {
+		failure      bool
+		name         string
+		skipCreation bool
+		want         bool
+	}{
+		{
+			failure:      false,
+			name:         "skip creation set to true",
+			skipCreation: true,
+			want:         true,
+		},
+		{
+			failure:      false,
+			name:         "skip creation set to false",
+			skipCreation: false,
+			want:         false,
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := WithSkipCreation(test.skipCreation)(e)
+
+			if test.failure {
+				if err == nil {
+					t.Errorf("WithSkipCreation for %s should have returned err", test.name)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Errorf("WithSkipCreation returned err: %v", err)
+			}
+
+			if !reflect.DeepEqual(e.config.SkipCreation, test.want) {
+				t.Errorf("WithSkipCreation is %v, want %v", e.config.SkipCreation, test.want)
+			}
+		})
+	}
+}
---
+++ database/executable/pop.go
@@ -0,0 +1,78 @@
+// Copyright (c) 2023 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package executable
+
+import (
+	"github.com/go-vela/types/constants"
+	"github.com/go-vela/types/database"
+	"github.com/go-vela/types/library"
+	"gorm.io/gorm/clause"
+)
+
+// PopBuildExecutable pops a build executable by build_id from the database.
+func (e *engine) PopBuildExecutable(id int64) (*library.BuildExecutable, error) {
+	e.logger.Tracef("popping build executable for build %d from the database", id)
+
+	// variable to store query results
+	b := new(database.BuildExecutable)
+
+	// at the time of coding, GORM does not implement a version of Sqlite3 that supports RETURNING.
+	// so we have to select and delete for the Sqlite driver.
+	switch e.config.Driver {
+	case constants.DriverPostgres:
+		// send query to the database and store result in variable
+		err := e.client.
+			Table(constants.TableBuildExecutable).
+			Clauses(clause.Returning{}).
+			Where("build_id = ?", id).
+			Delete(b).
+			Error
+
+		if err != nil {
+			return nil, err
+		}
+
+	case constants.DriverSqlite:
+		// send query to the database and store result in variable
+		err := e.client.
+			Table(constants.TableBuildExecutable).
+			Where("id = ?", id).
+			Take(b).
+			Error
+		if err != nil {
+			return nil, err
+		}
+
+		// send query to the database to delete result just got
+		err = e.client.
+			Table(constants.TableBuildExecutable).
+			Delete(b).
+			Error
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// decrypt the fields for the build executable
+	//
+	// https://pkg.go.dev/github.com/go-vela/types/database#Repo.Decrypt
+	err := b.Decrypt(e.config.EncryptionKey)
+	if err != nil {
+		return nil, err
+	}
+
+	// decompress data for the build executable
+	//
+	// https://pkg.go.dev/github.com/go-vela/types/database#BuildExecutable.Decompress
+	err = b.Decompress()
+	if err != nil {
+		return nil, err
+	}
+
+	// return the decompressed build executable
+	//
+	// https://pkg.go.dev/github.com/go-vela/types/database#BuildExecutable.ToLibrary
+	return b.ToLibrary(), nil
+}
