fcda6aaa133283c73ec966d1be61cd294fa90f01
Easton Crupper <65553218+ecrupper@users.noreply.github.com>
2023-09-07 16:08:54 -0400

    patch(webhook): support transfer and rename (#959)

---
+++ database/hook/get_webhook.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2023 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package hook
+
+import (
+	"github.com/go-vela/types/constants"
+	"github.com/go-vela/types/database"
+	"github.com/go-vela/types/library"
+)
+
+// GetHookByWebhookID gets a single hook with a matching webhook id in the database.
+func (e *engine) GetHookByWebhookID(webhookID int64) (*library.Hook, error) {
+	e.logger.Tracef("getting a hook with webhook id %d from the database", webhookID)
+
+	// variable to store query results
+	h := new(database.Hook)
+
+	// send query to the database and store result in variable
+	err := e.client.
+		Table(constants.TableHook).
+		Where("webhook_id = ?", webhookID).
+		Take(h).
+		Error
+	if err != nil {
+		return nil, err
+	}
+
+	// return the hook
+	//
+	// https://pkg.go.dev/github.com/go-vela/types/database#Hook.ToLibrary
+	return h.ToLibrary(), nil
+}
