b33ee01002a0c22379fbeac508f0d7bc05b37ae1
ecrupper <easton.crupper12@gmail.com>
2024-09-04 08:51:52 -0500

    enhance(mock): add admin key rotation

---
+++ mock/server/rotate_keys.go
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package server
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-vela/server/router/middleware/auth"
+	"github.com/go-vela/types"
+)
+
+// rotateKeys returns success message. Pass `invalid` to auth header to test 401 error.
+func rotateKeys(c *gin.Context) {
+	tkn, _ := auth.RetrieveAccessToken(c.Request)
+
+	if strings.EqualFold(tkn, "invalid") {
+		data := "unauthorized"
+		c.AbortWithStatusJSON(http.StatusUnauthorized, types.Error{Message: &data})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, "keys rotated successfully")
+}
