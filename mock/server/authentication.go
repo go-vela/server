b998ec11d9ad140a0748e311d7939c6adea02b8e
Jordan Sussman <jordan_sussman@cargill.com>
2024-09-05 16:32:49 -0500

    enhance(oidc): add more claims (#1172)

--- mock/server/authentication.go
+++ mock/server/authentication.go
@@ -32,6 +32,7 @@ const (
     "iat",
     "iss",
     "aud",
+    "branch",
     "build_number",
     "build_id",
     "repo",
@@ -40,6 +41,8 @@ const (
     "actor_scm_id",
     "commands",
     "image",
+    "image_name",
+    "image_tag",
     "request"
   ],
   "id_token_signing_alg_values_supported": [
