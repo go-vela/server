ada42d56a7837cbe51943dd27f1252f748d300e0
David May <49894298+wass3rw3rk@users.noreply.github.com>
2024-12-27 09:53:04 -0600

    refactor(db/build): drop source index, add event index (#1228)
    Co-authored-by: david may <1301201+wass3r@users.noreply.github.com>

--- database/resource_test.go
+++ database/resource_test.go
@@ -35,8 +35,8 @@ func TestDatabase_Engine_NewResources(t *testing.T) {
 	// ensure the mock expects the build queries
 	_mock.ExpectExec(build.CreatePostgresTable).WillReturnResult(sqlmock.NewResult(1, 1))
 	_mock.ExpectExec(build.CreateCreatedIndex).WillReturnResult(sqlmock.NewResult(1, 1))
+	_mock.ExpectExec(build.CreateEventIndex).WillReturnResult(sqlmock.NewResult(1, 1))
 	_mock.ExpectExec(build.CreateRepoIDIndex).WillReturnResult(sqlmock.NewResult(1, 1))
-	_mock.ExpectExec(build.CreateSourceIndex).WillReturnResult(sqlmock.NewResult(1, 1))
 	_mock.ExpectExec(build.CreateStatusIndex).WillReturnResult(sqlmock.NewResult(1, 1))
 	// ensure the mock expects the dashboard queries
 	_mock.ExpectExec(dashboard.CreatePostgresTable).WillReturnResult(sqlmock.NewResult(1, 1))
