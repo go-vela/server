9a532cf3aec149bce77e7be97bd1e51d188fa309
Easton Crupper <65553218+ecrupper@users.noreply.github.com>
2025-01-09 16:00:25 -0600

    enhance(metrics): include route specific queue length (#1237)

---
+++ queue/redis/route_length.go
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package redis
+
+import (
+	"context"
+)
+
+// RouteLength returns count of all items present in the given route.
+func (c *client) RouteLength(ctx context.Context, channel string) (int64, error) {
+	c.Logger.Tracef("reading length of all configured routes in queue")
+
+	items, err := c.Redis.LLen(ctx, channel).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	return items, nil
+}
