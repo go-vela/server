9ff830536234b2958b10168952cca4db77ccf7f0
TimHuynh <tim.huynh@target.com>
2025-04-11 13:33:40 -0500

    change comments

--- storage/minio/upload.go
+++ storage/minio/upload.go
@@ -16,7 +16,9 @@ import (
 // Upload uploads an object to a bucket in MinIO.ts.
 func (c *Client) Upload(ctx context.Context, object *api.Object) error {
 	c.Logger.Tracef("uploading data to bucket %s", object.Bucket.BucketName)
-	_, err := c.client.FPutObject(ctx, object.Bucket.BucketName, object.ObjectName, object.FilePath, minio.PutObjectOptions{})
+	info, err := c.client.FPutObject(ctx, object.Bucket.BucketName, object.ObjectName, object.FilePath, minio.PutObjectOptions{})
+
+	c.Logger.Infof("uploaded object %v with size %d", info, info.Size)
 
 	return err
 }
