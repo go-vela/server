b565d0bdedb634c9b17b5cfa9796666ee0612476
ecrupper <easton.crupper12@gmail.com>
2025-04-29 09:05:42 -0500

    remove middleware for role maps since they are now baked into scm service

--- cmd/vela-server/server.go
+++ cmd/vela-server/server.go
@@ -188,7 +188,6 @@ func server(ctx context.Context, cmd *cli.Command) error {
 		middleware.TokenManager(tm),
 		middleware.Queue(queue),
 		middleware.RequestVersion,
-		middleware.RoleMaps(cmd.StringMap("scm.repo.roles-map"), cmd.StringMap("scm.org.roles-map"), cmd.StringMap("scm.team.roles-map")),
 		middleware.Secret(cmd.String("vela-secret")),
 		middleware.Secrets(secrets),
 		middleware.Scm(scm),
