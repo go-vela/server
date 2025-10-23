3790af6cb5db497481442eecf59c723a1866a1d3
TimHuynh <tim.huynh@target.com>
2025-10-23 13:14:34 -0500

    refactor

--- storage/storage.go
+++ storage/storage.go
@@ -37,21 +37,25 @@ func FromCLICommand(_ context.Context, c *cli.Command) (Storage, error) {
 func New(s *Setup) (Storage, error) {
 	// validate the setup being provided
 	//
-	err := s.Validate()
-	if err != nil {
-		return nil, fmt.Errorf("unable to validate storage setup: %w", err)
-	}
+	// https://pkg.go.dev/github.com/go-vela/server/storage#Setup.Validate
+	if s.Enable {
+		err := s.Validate()
+		if err != nil {
+			return nil, fmt.Errorf("unable to validate storage setup: %w", err)
+		}
 
-	logrus.Debug("creating storage client from setup")
-	// process the storage driver being provided
-	switch s.Driver {
-	case constants.DriverMinio:
-		// handle the Kafka queue driver being provided
-		//
-		// https://pkg.go.dev/github.com/go-vela/server/queue?tab=doc#Setup.Kafka
-		return s.Minio()
-	default:
-		// handle an invalid queue driver being provided
-		return nil, fmt.Errorf("invalid storage driver provided: %s", s.Driver)
+		logrus.Debug("creating storage client from setup")
+		// process the storage driver being provided
+		switch s.Driver {
+		case constants.DriverMinio:
+			// handle the Kafka queue driver being provided
+			//
+			// https://pkg.go.dev/github.com/go-vela/server/queue?tab=doc#Setup.Kafka
+			return s.Minio()
+		default:
+			// handle an invalid queue driver being provided
+			return nil, fmt.Errorf("invalid storage driver provided: %s", s.Driver)
+		}
 	}
+	return nil, nil
 }
