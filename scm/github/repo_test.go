59ddd4d5ffeb3eb5eabb9d5a54c2157b5e868dda
ecrupper <easton.crupper12@gmail.com>
2026-01-07 14:32:29 -0600

    chore: bump go-github to v81

--- scm/github/repo_test.go
+++ scm/github/repo_test.go
@@ -15,7 +15,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v74/github"
+	"github.com/google/go-github/v81/github"
 
 	api "github.com/go-vela/server/api/types"
 	"github.com/go-vela/server/compiler/types/yaml"
